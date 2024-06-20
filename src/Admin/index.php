de5ec9e721c2c19f37f8a4a74a365f1f2f934c70
Hazz-i <wahidh697@gmail.com>
2024-06-20 21:34:02 +0700

    add: logout

--- src/Admin/index.php
+++ src/Admin/index.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin</title>
+    <link href="../main.css" rel="stylesheet">
 </head>
 <body>
-    <h1>INI DASHBOARD</h1>
+    <div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+        <!-- Page content here -->
+        <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+    </div> 
+    <div class="drawer-side">
+        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+        
+        </ul>
+    </div>
+    </div>
+
+    <a class="btn" href="logout.php">Logout</a>
 </body>
 </html>
